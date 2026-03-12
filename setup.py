9e3244441cf984385b89a88a215a0ae4b9d7d8b2
Shauryaman Sawai <sawaisushil@gmail.com>
2026-03-12 16:45:32 +0530

    changes_2_vercel

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='RAG_Document_Retrieval',
-    version='1.0.0',
+    name="RAG_Document_Retrieval",
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
+    license="MIT",
     install_requires=[
-        'django',
-        
+        "django>=4.2",
     ],
-    entry_points={
-        'console_scripts': [
-            'manage.py=manage:main',
-        ],
-    },
 )
